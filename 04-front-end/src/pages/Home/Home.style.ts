ce162568cdd6c714ed9c3cc0771c67bf65115507
vsimonovski <vsimonovski@outlook.com>
2021-06-22 20:37:40 +0200

    refactor(Home): reorganize Home screen component, add antd

--- 04-front-end/src/pages/Home/Home.style.ts
+++ 04-front-end/src/pages/Home/Home.style.ts
@@ -1,25 +1,57 @@
 import styled from 'styled-components';
 import { Container } from '../../components/App/App.style';
 import { appConfiguration } from '../../config/app.config';
-import {StyleConfiguration} from "../../config/app.config.type";
+import { StyleConfiguration } from '../../config/app.config.type';
 
 const styles: StyleConfiguration = appConfiguration.styles;
 
 const Title = styled.h1`
     text-transform: uppercase;
     text-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
-    font-style: italic;
-    color: #fff;
+    color: #333;
     font-size: 80px;
     font-family: ${styles.fonts.montserrat};
+    font-weight: 800;
     margin: 0;
+`;
+
+const Menu = styled.div`
+    display: flex;
+    flex-direction: column;
+    background: rgba(0, 0, 0, 0.2);
+    height: 60px;
+    justify-content: center;
+`;
+
+const ButtonWrap = styled.div`
+    align-self: flex-end;
+    Button {
+        margin: 0 10px;
+    }
+`;
+
+const Content = styled.div`
+    display: flex;
+    flex: auto;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        Button {
+            margin: 10px 0;
+            &:last-child {
+                margin: 0;
+            }
+        }
+    }
 `;
 
 const HomeContainer = styled(Container)`
     display: flex;
-    justify-content: center;
-    align-items: center;
     flex-direction: column;
 `;
 
-export { Title, HomeContainer };
+export { Title, HomeContainer, Menu, ButtonWrap, Content };
